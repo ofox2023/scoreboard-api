5a12171f237098488222637c591273627598ea9b
Harsh Patel <thepatel5324@gmail.com>
2024-01-23 12:05:14 +0530

    get match player api issue solved

--- routes/get/getMatchPlayers.js
+++ routes/get/getMatchPlayers.js
@@ -16,26 +16,41 @@ const getMatchPlayers = async (req, resp) => {
             data: [teamB],
         } = await getData(null, "teams", `_id = ${matchData.teamB}`);
 
-        teamA.anouncedPlayers = JSON.parse(teamA.anouncedPlayers);
-        teamA.subsitutePlayers = JSON.parse(teamA.subsitutePlayers);
+        if (teamA.anouncedPlayers && teamB.anouncedPlayers) {
+            teamA.anouncedPlayers =
+                teamA.anouncedPlayers && JSON.parse(teamA.anouncedPlayers);
+            teamA.subsitutePlayers =
+                teamA.subsitutePlayers && JSON.parse(teamA.subsitutePlayers);
 
-        teamB.anouncedPlayers = JSON.parse(teamB.anouncedPlayers);
-        teamB.subsitutePlayers = JSON.parse(teamB.subsitutePlayers);
+            teamB.anouncedPlayers =
+                teamB.anouncedPlayers && JSON.parse(teamB.anouncedPlayers);
+            teamB.subsitutePlayers =
+                teamB.subsitutePlayers && JSON.parse(teamB.subsitutePlayers);
 
-        teamA.nPlayers = JSON.parse(teamA.players).filter(
-            (i) =>
-                !teamA.anouncedPlayers.includes(i) &&
-                !teamA.subsitutePlayers.includes(i)
-        );
+            teamA.nPlayers = JSON.parse(teamA.players).filter(
+                (i) =>
+                    !teamA.anouncedPlayers.includes(i) &&
+                    !teamA.subsitutePlayers.includes(i)
+            );
 
-        teamB.nPlayers = JSON.parse(teamB.players).filter(
-            (i) =>
-                !teamB.anouncedPlayers.includes(i) &&
-                !teamB.subsitutePlayers.includes(i)
-        );
+            teamB.nPlayers = JSON.parse(teamB.players).filter(
+                (i) =>
+                    !teamB.anouncedPlayers.includes(i) &&
+                    !teamB.subsitutePlayers.includes(i)
+            );
 
-        teamA.players = [...teamA.anouncedPlayers, ...teamA.subsitutePlayers];
-        teamB.players = [...teamB.anouncedPlayers, ...teamB.subsitutePlayers];
+            teamA.players = [
+                ...teamA.anouncedPlayers,
+                ...teamA.subsitutePlayers,
+            ];
+            teamB.players = [
+                ...teamB.anouncedPlayers,
+                ...teamB.subsitutePlayers,
+            ];
+        } else {
+            teamA.anouncedPlayers = JSON.parse(teamA.players);
+            teamB.anouncedPlayers = JSON.parse(teamB.players);
+        }
 
         const teamAPlayers = [];
 
